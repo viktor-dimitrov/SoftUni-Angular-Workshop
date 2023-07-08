21492f110c4799c6d64743120809d705a3862308
viktor-dimitrov <106183268+viktor-dimitrov@users.noreply.github.com>
2023-07-08 12:44:02 +0300

    Implement data and add loader

--- src/app/posts-list/posts-list.component.ts
+++ src/app/posts-list/posts-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit} from '@angular/core';
 import { ApiService } from '../api.service';
+import { Post } from '../types/posts';
 
 
 @Component({
@@ -9,11 +10,22 @@ import { ApiService } from '../api.service';
 })
 export class PostsListComponent implements OnInit {
 
+  postsList: Post[] = [];
+  isLoading: boolean = true;
+
     constructor(private apiService: ApiService ) {}
 
     ngOnInit(): void {
-      this.apiService.getPosts(5).subscribe((posts)=>{
-        console.log({posts})
+      this.apiService.getPosts(5).subscribe({
+       next: (posts)=> {
+        this.postsList = posts;
+        this.isLoading = false;
+       },
+       error: (err) => {
+        this.isLoading = false;
+        console.error(`Error: ${err}`);
+       } 
+        
       })
     }
 
--- src/app/themes-list/themes-list.component.ts
+++ src/app/themes-list/themes-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../api.service';
+import { Theme } from '../types/themes';
 
 
 
@@ -9,12 +10,26 @@ import { ApiService } from '../api.service';
   styleUrls: ['./themes-list.component.css']
 })
 export class ThemesListComponent implements OnInit{
+
+  themesList: Theme[] = [];
+  isLoading: boolean = true;
+
+  
   constructor(private apiService: ApiService) {}
 
   ngOnInit(): void {
-    this.apiService.getThemes().subscribe((themes) => {
-      console.log({themes})
-    })
+    this.apiService.getThemes().subscribe({
+      next: (posts)=> {
+       this.themesList = posts;
+       this.isLoading = false;
+      },
+      error: (err) => {
+       this.isLoading = false;
+       console.error(`Error: ${err}`);
+
+      } 
+       
+     })
   }
 
 }
